3e0ea54ac22deedd7b73af6d626b57d9d233a153
David <dav-ss@mail.ru>
2019-05-14 11:38:50 -0700

    creating dav-utils module creating config package will contain screen size and so on.. desktop configuration

---
+++ core/src/com/davidvardanyan/brickbreaker/config/GameConfig.java
@@ -0,0 +1,16 @@
+package com.davidvardanyan.brickbreaker.config;
+
+public final class GameConfig {
+
+    //constants
+    //For desktop
+   public static final int WIDTH = 960;
+    public static final int HEIGHT = 768;
+
+    //World units
+    public static final float WORLD_WIDTH = 30f;
+    public static final float WORLD_HEIGHT = 24f;
+
+    public static final float WORLD_CENTER_X = WORLD_WIDTH /2f;
+    public static final float WORLD_CENTER_y = WORLD_HEIGHT /2f;
+}
--- desktop/src/com/davidvardanyan/brickbreaker/desktop/DesktopLauncher.java
+++ desktop/src/com/davidvardanyan/brickbreaker/desktop/DesktopLauncher.java
@@ -3,10 +3,13 @@ package com.davidvardanyan.brickbreaker.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.davidvardanyan.brickbreaker.BrickBreakerGame;
+import com.davidvardanyan.brickbreaker.config.GameConfig;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new BrickBreakerGame(), config);
+		config.width = GameConfig.WIDTH;
+		config.height = GameConfig.HEIGHT;
 	}
 }
