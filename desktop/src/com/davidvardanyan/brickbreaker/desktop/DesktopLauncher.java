8136befb2d58e65b6bc403fdecc846d1df654279
David <dav-ss@mail.ru>
2019-05-14 10:52:02 -0700

    Starting the project. Building the project with libgdx.

---
+++ desktop/src/com/davidvardanyan/brickbreaker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.davidvardanyan.brickbreaker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.davidvardanyan.brickbreaker.BrickBreakerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BrickBreakerGame(), config);
+	}
+}
